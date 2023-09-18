28d1925237857567e4c804d1cd57e8a153c1782c
hvkasu <kajol.sutradhar.fi>
2023-09-18 19:26:36 +0300

    Version 1.0.0

---
+++ main/js/srcipts.js
@@ -0,0 +1,84 @@
+let score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    losses: 0,
+    ties: 0
+};
+updateScoreElement();
+
+function playGames(playerMove) {
+    const computerMove = pickComputerMove();
+
+    let result = "";
+    if (playerMove == "scissors") {
+        if (computerMove === "rock") {
+            result = "You lose";
+        } else if (computerMove === "paper") {
+            result = "You Win";
+        } else if (computerMove === "scissors") {
+            result = "Tie";
+        }
+    } else if (playerMove === "paper") {
+        if (computerMove === "rock") {
+            result = "You Win";
+        } else if (computerMove === "paper") {
+            result = "Tie";
+        } else if (computerMove === "scissors") {
+            result = "You lose";
+        }
+    } else if (playerMove === "rock") {
+        if (computerMove === "rock") {
+            result = "Tie";
+        } else if (computerMove === "paper") {
+            result = "You lose";
+        } else if (computerMove === "scissors") {
+            result = "You Win";
+        }
+    }
+
+    if (result === 'You Win') {
+        score.wins += 1;
+    } else if (result === "You lose") {
+        score.losses += 1;
+    } else if (result === 'Tie') {
+        score.ties += 1;
+    };
+
+
+    localStorage.setItem('score', JSON.stringify(score));
+    updateScoreElement();
+
+    document.querySelector('.js-result').innerHTML = result;
+
+    document.querySelector('.js-moves').innerHTML = `You <img src="./main/img/${playerMove}-emoji.png" alt="rock" class="move-icon">
+    <img src="./main/img/${computerMove}-emoji.png" alt="" class="move-icon">
+     Comp`
+
+}
+
+function updateScoreElement() {
+    document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}.
+`;
+}
+
+function resetScore() {
+    score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+    localStorage.removeItem('score')
+    updateScoreElement();
+}
+// cons 
+
+function pickComputerMove() {
+    let computerMove = "";
+    const randomNumber = Math.random();
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+        computerMove = "rock";
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+        computerMove = "paper";
+    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+        computerMove = "scissors";
+    }
+
+    return computerMove;
+}
